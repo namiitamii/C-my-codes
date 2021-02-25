619ec4c807ef92f151eee34e42aa269b6a2cfcc3
namiitamii <79623695+namiitamii@users.noreply.github.com>
2021-02-24 21:46:53 -0800

    Homework1
    Feb.22.2021

---
+++ homeworkHicheel1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+	
+	int x = 1000;
+	double y = 100.99;
+	
+    double z=x+y;
+	
+	cout<<"z=";
+	cout<<z<<endl;
+	cout<<endl;
+	
+	double a = 10000;
+	double b = 1298.723;
+	
+	double c = a-b;
+	
+	cout<<"c=";
+	cout<<c<<endl;
+	cout<<endl;
+	
+	int q = 10 , p = 100;
+	
+	int m = p/q;
+	
+	cout<<"m=";
+	cout<<m<<endl;
+	cout<<endl;
+	
+	
+	return 0;	
+}
