d1c5089dd49eda9d80dce1664a067851a5dffd79
namiitamii <79623695+namiitamii@users.noreply.github.com>
2021-03-03 18:48:23 -0800

    Mar/01/21 homework Exercise10 of w3resource.com C+
    10. Write a C program that accepts two integers from the user and calculate the product of the two integers. Go to the editor
    Test Data :
    Input the first integer: 25
    Input the second integer: 15
    Expected Output:
    Product of the above two integers = 375

---
+++ w3resource.com10.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+	int x , y;
+	
+	
+	cout<<"Enter x: ";
+	cin>>x;
+	cout<<"Enter y: ";
+	cin>>y;
+	
+	cout<<"Product of the above two integers: "<<x*y;
+	
+		
+	return 0;	
+}
