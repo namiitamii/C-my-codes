c31ec43bf80c045503f2b61b33fd2d034a06ea16
namiitamii <79623695+namiitamii@users.noreply.github.com>
2021-03-03 17:35:56 -0800

    feb,24,2021 hicheelin homework
    Homework of 02/24/2021

---
+++ homeworkexercise1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+	string x = "C";
+	string y = "-32768";
+	
+	cout<<"Enter an character: ";
+	cin>>x;
+	if (x == "C"){
+		cout<<"You entered character: 'C'"<<endl;
+		}
+	
+	cout<<"Enter a signed short value: ";
+	cin>>y;
+	if (y == "-32768"){
+		cout<<"You entered signed short: -32768";
+		}
+	
+	
+			
+	return 0;	
+}
---
+++ homeworkexercise3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+	int x , y , a , b , c , d , e;
+	cout<<"Enter First number: ";
+	cin>>x;
+	cout<<"Enter Second number: ";
+	cin>>y;
+	
+	a = x + y;
+	b = x - y;
+	c = x * y;
+	d = x / y;
+	e = x % y;
+	
+	cout<<"x + y = "<<a<<endl;
+	cout<<"x - y = "<<b<<endl;
+	cout<<"x * y = "<<c<<endl;
+	cout<<"x / y = "<<d<<endl;
+	cout<<"x & y = "<<e<<endl;
+	
+		
+	return 0;	
+}
